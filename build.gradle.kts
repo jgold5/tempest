b29390dea67deb41cf576d1bf8e70b86b44c07f6
jgold5 <jgold885@gmail.com>
2026-03-08 18:12:49 -0400

    chore: setup sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,21 @@ plugins {
     java
     jacoco
     id("com.diffplug.spotless") version "6.23.0"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "com.github.jgold5"
 version = "1.0-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "jgold5_tempest")
+        property("sonar.organization", "jgold5")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths",
+            "tempest-core/build/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
 
 subprojects {
     apply(plugin = "java")
