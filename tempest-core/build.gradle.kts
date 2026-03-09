fbe96409c664fddfcf505488bcde3fbd06278c7f
jgold5 <jgold885@gmail.com>
2026-03-08 20:53:34 -0400

    feature: single http request

--- tempest-core/build.gradle.kts
+++ tempest-core/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+    implementation(platform(libs.okhttp))
     testImplementation(platform(libs.junit.bom))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
